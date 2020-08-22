cb162b25fad5b747c7f6bf76d541275992c92081
Bruno Alves de Faria <bruno.faria@radicalimaging.com>
2020-08-21 22:41:18 -0300

    WIP: Refactoring code and creating global flag for clipping

--- src/drawing/drawLinkedTextBox.js
+++ src/drawing/drawLinkedTextBox.js
@@ -1,6 +1,8 @@
 import external from '../externalModules.js';
 import drawTextBox from './drawTextBox.js';
 import drawLink from './drawLink.js';
+import { state } from './../store/index.js';
+import { clipBoxToDisplayedArea } from '../util/clip.js';
 
 /**
  * Draw a link between an annotation to a textBox.
@@ -32,87 +34,26 @@ export default function(
   xOffset,
   yCenter
 ) {
-  const cornerstone = external.cornerstone;
+  const { pixelToCanvas } = external.cornerstone;
 
   // Convert the textbox Image coordinates into Canvas coordinates
-  const textCoords = cornerstone.pixelToCanvas(element, textBox);
+  const textCoords = pixelToCanvas(element, textBox);
 
   if (xOffset) {
     textCoords.x += xOffset;
   }
-
-  const getBoxPixelLimits = (element, box) => {
-    const toPixel = point => cornerstone.canvasToPixel(element, point);
-    const { top, left, width, height } = box;
-    const topLeft = toPixel({ x: left, y: top });
-    const topRight = toPixel({ x: left + width, y: top });
-    const bottomLeft = toPixel({ x: left, y: top + height });
-    const bottomRight = toPixel({ x: left + width, y: top + height });
-    const points = [topLeft, topRight, bottomLeft, bottomRight];
-    const xArray = points.map(p => p.x);
-    const yArray = points.map(p => p.y);
-
-    return {
-      minX: Math.min(...xArray),
-      minY: Math.min(...yArray),
-      maxX: Math.max(...xArray),
-      maxY: Math.max(...yArray),
-    };
-  };
-
-  const clipBoxOnAxis = (point, axis, min, max, upper, lower) => {
-    // Reposition bounding box in the given axis of the displayed area
-    if (lower - upper < max - min) {
-      // Box bigger than displayed area
-      point[axis] += upper - min; // Stick to the upper boundary
-      point[axis] += (lower - upper) / 2; // Centralize in displayed area
-      point[axis] -= (max - min) / 2; // Subtract 1/2 box's height
-    } else if (min < upper) {
-      // Leaked displayed area's upper boundary
-      point[axis] += upper - min; // Stick to the upper boundary
-    } else if (max > lower) {
-      // Leaked displayed area's lower boundary
-      point[axis] -= max - lower; // Stick to the lower boundary
-    }
-  };
 
   const options = {
     centering: {
       x: false,
       y: yCenter,
     },
-    translator(boundingBox) {
-      const { pixelToCanvas, canvasToPixel, getViewport } = cornerstone;
-      const pixelPosition = canvasToPixel(element, {
-        x: boundingBox.left,
-        y: boundingBox.top,
-      });
-      const { minX, minY, maxX, maxY } = getBoxPixelLimits(
-        element,
-        boundingBox
-      );
-
-      // Get the displayed area's top, left, bottom and right boundaries
-      const { tlhc, brhc } = getViewport(element).displayedArea;
-      const top = tlhc.y - 1;
-      const left = tlhc.x - 1;
-      const bottom = brhc.y;
-      const right = brhc.x;
-
-      // Clip the bounding box on vertical axis
-      clipBoxOnAxis(pixelPosition, 'y', minY, maxY, top, bottom);
-
-      // Clip the bounding box on horizontal axis
-      clipBoxOnAxis(pixelPosition, 'x', minX, maxX, left, right);
-
-      // Transform the bounding box coordinate system back to canvas
-      const newCanvasPosition = pixelToCanvas(element, pixelPosition);
-
-      // Update the bounding box with the new coordinates
-      boundingBox.top = newCanvasPosition.y;
-      boundingBox.left = newCanvasPosition.x;
-    },
   };
+
+  // Clip the bounding box to the displayed area of the image
+  if (state.preventTextBoxOutsideDisplayedArea) {
+    options.translator = box => clipBoxToDisplayedArea(element, box);
+  }
 
   // Draw the text box
   textBox.boundingBox = drawTextBox(
@@ -126,7 +67,7 @@ export default function(
   if (textBox.hasMoved) {
     // Identify the possible anchor points for the tool -> text line
     const linkAnchorPoints = textBoxAnchorPoints(handles).map(h =>
-      cornerstone.pixelToCanvas(element, h)
+      pixelToCanvas(element, h)
     );
 
     // Draw dashed link line between tool and text
--- src/util/clip.js
+++ src/util/clip.js
@@ -1,3 +1,5 @@
+import external from '../externalModules.js';
+
 /**
  * Clips a value to an upper and lower bound.
  * @export @public @method
@@ -23,8 +25,76 @@ export function clip(val, low, high) {
  */
 export function clipToBox(point, box) {
   // Clip an {x, y} point to a box of size {width, height}
-  point.x = clip(point.x, 0, box.width);
-  point.y = clip(point.y, 0, box.height);
+  point.x = clip(point.x, box.left || 0, box.width);
+  point.y = clip(point.y, box.top || 0, box.height);
+}
+
+const getBoxPixelLimits = (element, box) => {
+  const toPixel = point => external.cornerstone.canvasToPixel(element, point);
+  const { top, left, width, height } = box;
+  const topLeft = toPixel({ x: left, y: top });
+  const topRight = toPixel({ x: left + width, y: top });
+  const bottomLeft = toPixel({ x: left, y: top + height });
+  const bottomRight = toPixel({ x: left + width, y: top + height });
+  const points = [topLeft, topRight, bottomLeft, bottomRight];
+  const xArray = points.map(p => p.x);
+  const yArray = points.map(p => p.y);
+
+  return {
+    minX: Math.min(...xArray),
+    minY: Math.min(...yArray),
+    maxX: Math.max(...xArray),
+    maxY: Math.max(...yArray),
+  };
+};
+
+const clipBoxOnAxis = (point, axis, min, max, upper, lower) => {
+  // Reposition bounding box in the given axis of the displayed area
+  if (lower - upper < max - min) {
+    // Box bigger than displayed area
+    point[axis] += upper - min; // Stick to the upper boundary
+    point[axis] += (lower - upper) / 2; // Centralize in displayed area
+    point[axis] -= (max - min) / 2; // Subtract 1/2 box's height
+  } else if (min < upper) {
+    // Leaked displayed area's upper boundary
+    point[axis] += upper - min; // Stick to the upper boundary
+  } else if (max > lower) {
+    // Leaked displayed area's lower boundary
+    point[axis] -= max - lower; // Stick to the lower boundary
+  }
+};
+
+export function clipBoxToDisplayedArea(element, box) {
+  const { pixelToCanvas, canvasToPixel, getViewport } = external.cornerstone;
+
+  // Transform the position of given box from canvas to pixel coordinates
+  const pixelPosition = canvasToPixel(element, {
+    x: box.left,
+    y: box.top,
+  });
+
+  // Get the rotated corners' position for the box in pixel coordinate system
+  const { minX, minY, maxX, maxY } = getBoxPixelLimits(element, box);
+
+  // Get the displayed area's top, left, bottom and right boundaries
+  const { tlhc, brhc } = getViewport(element).displayedArea;
+  const top = tlhc.y - 1;
+  const left = tlhc.x - 1;
+  const bottom = brhc.y;
+  const right = brhc.x;
+
+  // Clip the box on vertical axis
+  clipBoxOnAxis(pixelPosition, 'y', minY, maxY, top, bottom);
+
+  // Clip the box on horizontal axis
+  clipBoxOnAxis(pixelPosition, 'x', minX, maxX, left, right);
+
+  // Transform the box coordinate system back to canvas
+  const newCanvasPosition = pixelToCanvas(element, pixelPosition);
+
+  // Update the box with the new coordinates
+  box.top = newCanvasPosition.y;
+  box.left = newCanvasPosition.x;
 }
 
 export default clip;
